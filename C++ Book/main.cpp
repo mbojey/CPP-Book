01e1a75404de6346946b75d9e6529af53c3bdbb7
Matthew Bojey <Matt@Matthews-MacBook-Air.local>
2015-07-27 22:24:34 -0700

    Initial Commit

---
+++ C++ Book/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Book
+//
+//  Created by Matthew Bojey on 2015-07-27.
+//  Copyright (c) 2015 Matthew Bojey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
